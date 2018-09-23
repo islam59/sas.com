0500efbe51268557b7e162a98ae6a31b7db4bf14
islam59 <islamhossainwork@gmail.com>
2018-09-24 03:56:32 +0600

    home design done & attendance first page development done

--- Pages/Attendance.php
+++ Pages/Attendance.php
@@ -1,4 +1,4 @@
-<?php /*
+<?php 
   if(isset($_GET['update'])){
     $id = $_POST['id']; 
     $department_name = $_POST['department_name']; 
@@ -16,9 +16,9 @@
       }
     }
 
-  }*/
+  }
 ?>
-<?php /*
+<?php
   if(isset($_GET['remove'])){
     $id = $_GET['remove']; 
     //$department_name = $_POST['department_name'];
@@ -34,31 +34,47 @@
         header('Location:index.php?page=Departments&signal='.$signal);
       }
     }
-  }*/
+  }
 ?>
-<?php /*
+<?php 
 	if(isset($_GET['save'])){
-		$department_name = $FM->validation($_POST['department_name']);
-		$department_name =  mysqli_real_escape_string($DB->link,$department_name);
+		$dept_id = $FM->validation($_POST['dept_id']);
+		$dept_id =  mysqli_real_escape_string($DB->link,$dept_id);
 
-		if(empty($department_name)){
+    $course_id = $FM->validation($_POST['course_id']);
+    $course_id =  mysqli_real_escape_string($DB->link,$course_id);
+
+    $profile_id = $FM->validation($_POST['profile_id']);
+    $profile_id =  mysqli_real_escape_string($DB->link,$profile_id);
+
+    $room_no = $FM->validation($_POST['room_no']);
+    $room_no =  mysqli_real_escape_string($DB->link,$room_no);
+
+    $class_date = $FM->validation($_POST['class_date']);
+    $class_date =  mysqli_real_escape_string($DB->link,$class_date);
+
+    $class_time = $FM->validation($_POST['class_time']);
+    $class_time =  mysqli_real_escape_string($DB->link,$class_time);
+
+		if(empty($dept_id) OR empty($course_id) OR empty($profile_id) OR empty($room_no) OR empty($class_date) OR empty($class_time)){
 			$signal = '<small class="glyphicon glyphicon-remove" aria-hidden="true" style="color:red;"></small>';  
-       header('Location:index.php?page=Departments&signal='.$signal);
+       header('Location:index.php?page=Attendance&signal='.$signal);
 		}else{
-			$qry = "INSERT INTO tb_department (department_name) VALUES ('$department_name')"; 
+			$qry = "INSERT INTO tb_schedule (dept_id,course_id,profile_id,room_no,class_date,class_time) VALUES ('$dept_id','$course_id','$profile_id','$room_no','$class_date','$class_time')"; 
+
 			$result = $DB->insert($qry); 
 			if($result){
 				$signal = '<small class="glyphicon glyphicon-ok" aria-hidden="true" style="color:green;"></small>'; 
-        header('Location:index.php?page=Departments&signal='.$signal);  	
+        header('Location:index.php?page=Attendance&signal='.$signal);  	
 			}
 		}
-	}*/
+	}
 ?>
 <section class="container-fluid section-content">
 	<section class="container">
-		<h3>Department Control Panel &nbsp; <?php if(isset($_GET['signal'])){ echo $_GET['signal']; }?>
-			<button type="button" class="btn btn-primary btn-sm pull-right" data-toggle="modal" data-target="#addDepartment">
-  				+ Add Departments
+		<h3>Schedule Control Panel &nbsp; <?php if(isset($_GET['signal'])){ echo $_GET['signal']; }?>
+			<button type="button" class="btn btn-primary btn-sm pull-right" data-toggle="modal" data-target="#addSchedule">
+  				+ Add Schedule
 			</button>
 		</h3><hr/>
 <!--/=================================================================/-->
@@ -66,27 +82,103 @@
 
 
 <!-- Modal -->
-<div class="modal fade" id="addDepartment" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal fade" id="addSchedule" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
-<form class="form-horizontal" action="index.php?page=Departments&save" method="post">
+<form class="form-horizontal" action="index.php?page=Attendance&save" method="post">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Add Department</h4>
+        <h4 class="modal-title" id="myModalLabel">Add Schedule</h4>
       </div>
       <div class="modal-body">
 
   <div class="form-group">
-    <label for="inputEmail3" class="col-sm-2 control-label">Department</label>
-    <div class="col-sm-10">
-      <input name="department_name" type="text" class="form-control" id="inputEmail3" placeholder="Enter Department Name !">
+    <label for="inputEmail3" class="col-sm-3 control-label">Department</label>
+    <div class="col-sm-9">
+      <select name="dept_id" class="form-control">
+        <option value="">Select Department</option>
+<?php 
+  $qry = "SELECT * FROM tb_department WHERE status=0";
+  $result = $DB->select($qry); 
+  if($result){
+    while($dept = $result->fetch_array()){
+?>
+        <option value="<?php echo $dept['id']; ?>"><?php echo $dept['department_name']; ?></option>
+<?php 
+    }//end of dept while..
+  }//end of dept if..
+?>
+      </select>
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-3 control-label">Course</label>
+    <div class="col-sm-9">
+      <select name="course_id" class="form-control">
+        <option value="">Select Course</option>
+<?php 
+  $qryc = "SELECT * FROM tb_course WHERE status=0";
+  $resultc = $DB->select($qryc); 
+  if($resultc){
+    while($c = $resultc->fetch_array()){
+?>
+        <option value="<?php echo $c['id']; ?>"><?php echo $c['course_name']; ?></option>
+<?php 
+    }//end of c while..
+  }//end of c if..
+?>
+      </select>
+    </div>
+  </div>
+
+
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-3 control-label">Teacher</label>
+    <div class="col-sm-9">
+      <select name="profile_id" class="form-control">
+        <option value="">Select Teacher</option>
+<?php 
+  $qryp = "SELECT * FROM tb_profile WHERE status=0";
+  $resultp = $DB->select($qryp); 
+  if($resultp){
+    while($p = $resultp->fetch_array()){
+?>
+        <option value="<?php echo $p['id']; ?>"><?php echo $p['first_name'].''.$p['last_name']; ?></option>
+<?php 
+    }//end of c while..
+  }//end of c if..
+?>
+      </select>
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-3 control-label">Room No</label>
+    <div class="col-sm-9">
+      <input type="text" name="room_no" class="form-control" placeholder="Room No!">
+    </div>
+  </div>
+
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-3 control-label">Class Date</label>
+    <div class="col-sm-9">
+      <input type="date" name="class_date" class="form-control">
+    </div>
+  </div>
+
+
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-3 control-label">Class Time</label>
+    <div class="col-sm-9">
+      <input type="text" name="class_time" class="form-control">
     </div>
   </div>
 
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">+ Add Department</button>
+        <button type="submit" class="btn btn-primary">+ Add Schedule</button>
       </div>
 </form>
     </div>
@@ -94,33 +186,32 @@
 </div>
 <!--/=================================================================/-->
 
-
-
-
+<div class="col-md-12 col-sm-12 col-lg-12">
+  <div class="row">
 <?php 
-  $qry = "SELECT * FROM tb_schedule";
-  $result = $DB->select($qry); 
-  if($result){
-    $i = 0; 
-    while($dept = $result->fetch_array()){
-      $i++;
+  $qrySCH = "SELECT * FROM tb_schedule"; 
+  $resSCH = $DB->select($qrySCH); 
+  if($resSCH){
+    while($dataSCH = $resSCH->fetch_array()){
 ?>
-	<div class="col-sm-6 col-xs-6 col-md-2 col-lg-2 col-xl-2">;lsadfkj</div>
+<button class="btn btn-lg btn-primary" style="border-radius: 0; box-shadow: 0px 0px 5px red;">
+  <?php 
+    $profile = $dataSCH['profile_id']; 
+    $qprofile = "SELECT * FROM tb_profile WHERE id='$profile'"; 
+    $rprofile = $DB->select($qprofile);
+    if($rprofile = $rprofile->fetch_assoc()){
+      echo $rprofile['first_name'].' '.$rprofile['last_name'];
+    }
+  ?><br/>
+  <span style="color:red;"><?php echo $dataSCH['class_time']; ?></span><br/>
+  <?php echo $dataSCH['class_date']; ?>
+</button>
 <?php 
-    }//end of while
-  }//end of if..
+    }
+  }
 ?>
- 
-
-
-    </tbody>
-</table>
-<br/>
-<script>
-$(document).ready( function () {
-    $('#mytableId').DataTable();
-} );
-</script>
+  </div>
+</div>
 
 
 	</section>
--- Pages/Home.php
+++ Pages/Home.php
@@ -1,3 +1,41 @@
 <section class="container-fluid section-content">
-<h3>Home</h3><hr/>
+	<section class="container">
+		<h3>Home</h3><hr/>
+		<div class="col-md-6">
+			<div style="min-height: : 290vh; border:1px solid red;	text-align:center; font-size: 3em; margin-top:5px; margin-bottom:5px;">
+				502<BR/>
+				STUDENTS 
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div style="min-height: : 290vh; border:1px solid red;	text-align:center; font-size: 3em; margin-top:5px; margin-bottom:5px;">
+				502<BR/>
+				Teachers 
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div style="min-height: : 290vh; border:1px solid red;	text-align:center; font-size: 3em; margin-top:5px; margin-bottom:5px;">
+				502<BR/>
+				Attened In Class 
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div style="min-height: : 290vh; border:1px solid red;	text-align:center; font-size: 3em; margin-top:5px; margin-bottom:5px;">
+				502<BR/>
+				Absent Student 
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div style="min-height: : 290vh; border:1px solid red;	text-align:center; font-size: 3em; margin-top:5px; margin-bottom:5px;">
+				502<BR/>
+				Faculties
+			</div>
+		</div>
+		<div class="col-md-6">
+			<div style="min-height: : 290vh; border:1px solid red;	text-align:center; font-size: 3em; margin-top:5px; margin-bottom:5px;">
+				502<BR/>
+				Courses 
+			</div>
+		</div>
+	</section>
 </section>
