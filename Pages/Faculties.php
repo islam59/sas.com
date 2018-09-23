f65be341dc94496637af0e714a0b05c9fc9169d6
islam59 <islamhossainwork@gmail.com>
2018-09-24 01:36:09 +0600

    Faculties Module 100% updated

--- Pages/Faculties.php
+++ Pages/Faculties.php
@@ -3,56 +3,48 @@
 
     $uid = $_GET['update']; 
 
-    $name = $FM->validation($_POST['name']);
-    $name =  mysqli_real_escape_string($DB->link,$name);
+    $first_name = $FM->validation($_POST['first_name']);
+    $first_name =  mysqli_real_escape_string($DB->link,$first_name);
+
+    $last_name = $FM->validation($_POST['last_name']);
+    $last_name =  mysqli_real_escape_string($DB->link,$last_name);
+
+
+    $designation = $FM->validation($_POST['designation']);
+    $designation =  mysqli_real_escape_string($DB->link,$designation);
+
+
+    $education = $FM->validation($_POST['education']);
+    $education =  mysqli_real_escape_string($DB->link,$education);
+
+
+    $phone = $FM->validation($_POST['phone']);
+    $phone =  mysqli_real_escape_string($DB->link,$phone);
+
+
+    $email = $FM->validation($_POST['email']);
+    $email =  mysqli_real_escape_string($DB->link,$email);
 
     $dept_id = $FM->validation($_POST['dept_id']);
     $dept_id =  mysqli_real_escape_string($DB->link,$dept_id);
 
-
-    $batch = $FM->validation($_POST['batch']);
-    $batch =  mysqli_real_escape_string($DB->link,$batch);
-
-
-    $session = $FM->validation($_POST['session']);
-    $session =  mysqli_real_escape_string($DB->link,$session);
-
-
-    $birthdate = $FM->validation($_POST['birthdate']);
-    $birthdate =  mysqli_real_escape_string($DB->link,$birthdate);
-
-
-    $fathers_name = $FM->validation($_POST['fathers_name']);
-    $fathers_name =  mysqli_real_escape_string($DB->link,$fathers_name);
-
-    $mothers_name = $FM->validation($_POST['mothers_name']);
-    $mothers_name =  mysqli_real_escape_string($DB->link,$mothers_name);
-
-    $contact = $FM->validation($_POST['contact']);
-    $contact =  mysqli_real_escape_string($DB->link,$contact);
-
-    $address = $FM->validation($_POST['address']);
-    $address =  mysqli_real_escape_string($DB->link,$address);
-
-    if(empty($name) OR empty($dept_id) OR empty($session) OR empty($batch) OR empty($fathers_name) OR empty($mothers_name) OR empty($contact) OR empty($address)){
+    if(empty($first_name) OR empty($last_name) OR empty($designation) OR empty($education) OR empty($phone) OR empty($email) OR empty($dept_id)){
       $signal = '<small class="glyphicon glyphicon-remove" aria-hidden="true" style="color:yellow;"></small>';  
-       header('Location:index.php?page=Students&signal='.$signal);
+       header('Location:index.php?page=Faculties&signal='.$signal);
     }else{
-      $qry = "UPDATE tb_student 
+      $qry = "UPDATE tb_profile 
       SET 
-      name = '$name',
-      dept_id= '$dept_id',
-      batch = '$batch',
-      session = '$session',
-      birthdate = '$birthdate',
-      fathers_name = '$fathers_name',
-      mothers_name = '$mothers_name',
-      contact = '$contact',
-      address = '$address' WHERE id='$uid'"; 
+      first_name = '$first_name',
+      last_name= '$last_name',
+      designation = '$designation',
+      education = '$education',
+      phone = '$phone',
+      email = '$email',
+      dept_id = '$dept_id' WHERE id='$uid'"; 
       $resultu = $DB->update($qry); 
       if($resultu){
         $signal = '<small class="glyphicon glyphicon-ok" aria-hidden="true" style="color:yellow;"></small>'; 
-        header('Location:index.php?page=Students&signal='.$signal);    
+        header('Location:index.php?page=Faculties&signal='.$signal);    
       }
     }
   }
@@ -61,23 +53,24 @@
 <?php
   if(isset($_GET['remove'])){
     $id = $_GET['remove']; 
+    $uid = $_GET['uid'];
     //$department_name = $_POST['department_name'];
     
     if(empty($id)){
       $signal = '<small class="glyphicon glyphicon-remove" aria-hidden="true" style="color:red;"></small>';  
-       header('Location:index.php?page=Students&signal='.$signal);
+       header('Location:index.php?page=Faculties&signal='.$signal);
     }else{
       $qry = "DELETE FROM tb_profile WHERE id='$id'"; 
       $result = $DB->delete($qry); 
       if($result){
-  		$qry = "DELETE FROM tb_user WHERE id='$id'";
-  		$delres = $DB->delete('tb_user'); 
+  		$qryd = "DELETE FROM tb_user WHERE id='$uid'";
+  		$delres = $DB->delete($qryd); 
   		if($delres){
         	$signal = '<small class="glyphicon glyphicon-ok" aria-hidden="true" style="color:red;"></small>';  
-        	header('Location:index.php?page=Students&signal='.$signal);
+        	header('Location:index.php?page=Faculties&signal='.$signal);
   		}else{
         	$signal = '<small class="glyphicon glyphicon-ok" aria-hidden="true" style="color:red;"></small>';  
-        	header('Location:index.php?page=Students&signal='.$signal);
+        	header('Location:index.php?page=Faculties&signal='.$signal);
   		}
       }
     }
@@ -270,7 +263,7 @@ if($password = $c_password){
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">+ Add Student</button>
+        <button type="submit" class="btn btn-primary">+ Add Member</button>
       </div>
 </form>
     </div>
@@ -321,7 +314,15 @@ if($password = $c_password){
             </td>
             <td style="text-align: center;">
               <button class="btn btn-sm btn-warning"  data-toggle="modal" data-target="#updateFaculties<?php echo $profile['id']; ?>">Update</button>
-              <a href="index.php?page=Faculties&remove=<?php echo $profile['id']; ?>" class="btn btn-sm btn-danger">Remove</a>
+              <?php 
+                $pid = $profile['id'];
+                $userqry = "SELECT id from tb_user WHERE profile_id='$pid'"; 
+                $resuser = $DB->select($userqry); 
+                if($resuser = $resuser->fetch_assoc()){
+                  $id=$resuser['id'];
+                }
+              ?>
+              <a href="index.php?page=Faculties&uid=<?php echo $id; ?>&remove=<?php echo $profile['id'];?>" class="btn btn-sm btn-danger">Remove</a>
 
         <!-- Modal -->
         <div class="modal fade" id="updateFaculties<?php echo $profile['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
@@ -335,12 +336,48 @@ if($password = $c_password){
               <div class="modal-body" style="min-height: 10vh;">
 
           <div class="form-group">
-            <label for="inputEmail3" class="col-sm-3 control-label">Name</label>
+            <label for="inputEmail3" class="col-sm-3 control-label">First Name</label>
             <div class="col-sm-9">
               <input name="id" type="hidden" class="form-control" id="inputEmail3" value="<?php echo $profile['id']; ?>">
-              <input name="name" type="text" class="form-control" id="inputEmail3" value="<?php echo $profile['name']; ?>">
+              <input name="first_name" type="text" class="form-control" id="inputEmail3" value="<?php echo $profile['first_name']; ?>">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="inputEmail3" class="col-sm-3 control-label">Last Name</label>
+            <div class="col-sm-9">
+              <input name="id" type="hidden" class="form-control" id="inputEmail3" value="<?php echo $profile['id']; ?>">
+              <input name="last_name" type="text" class="form-control" id="inputEmail3" value="<?php echo $profile['last_name']; ?>">
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="inputEmail3" class="col-sm-3 control-label">Designation</label>
+            <div class="col-sm-9">
+              <input name="designation" type="text" class="form-control" id="inputEmail3" value="<?php echo $profile['designation']; ?>">
             </div>
           </div> 
+
+          <div class="form-group">
+            <label for="inputEmail3" class="col-sm-3 control-label">Education</label>
+            <div class="col-sm-9">
+              <input name="education" type="text" class="form-control" id="inputEmail3" value="<?php echo $profile['education']; ?>">
+            </div>
+          </div> 
+
+          <div class="form-group">
+            <label for="inputEmail3" class="col-sm-3 control-label">Phone</label>
+            <div class="col-sm-9">
+              <input name="phone" type="text" class="form-control" id="inputEmail3" value="<?php echo $profile['phone']; ?>">
+            </div>
+          </div> 
+
+          <div class="form-group">
+            <label for="inputEmail3" class="col-sm-3 control-label">Email</label>
+            <div class="col-sm-9">
+              <input name="email" type="text" class="form-control" id="inputEmail3" value="<?php echo $profile['email']; ?>">
+            </div>
+          </div> 
+
 
 <div class="form-group">
     <label for="inputEmail3" class="col-sm-3 control-label">Department</label>
@@ -353,7 +390,7 @@ if($password = $c_password){
   if($result){
     while($deptd = $resd->fetch_array()){
 ?>
-        <option value="<?php echo $deptd['id']; ?>" <?php if($student['dept_id'] == $deptd['id']){ echo 'selected'; } ?> ><?php echo $deptd['department_name']; ?></option>
+        <option value="<?php echo $deptd['id']; ?>" <?php if($profile['dept_id'] == $deptd['id']){ echo 'selected'; } ?> ><?php echo $deptd['department_name']; ?></option>
 <?php 
     }//end of dept while..
   }//end of dept if..
@@ -361,61 +398,10 @@ if($password = $c_password){
       </select>
     </div>
   </div>
-
-  <div class="form-group">
-    <label for="inputEmail3" class="col-sm-3 control-label">Batch</label>
-    <div class="col-sm-9">
-      <input name="batch" type="text" class="form-control" id="inputEmail3" value="<?php echo $student['batch']; ?>">
-    </div>
-  </div>
-
-  <div class="form-group">
-    <label for="inputEmail3" class="col-sm-3 control-label">Session</label>
-    <div class="col-sm-9">
-      <input name="session" type="text" class="form-control" id="inputEmail3" value="<?php echo $student['session']; ?>">
-    </div>
-  </div>
-
-  <div class="form-group">
-    <label for="inputEmail3" class="col-sm-3 control-label">Birthdate</label>
-    <div class="col-sm-9">
-      <input name="birthdate" type="date" class="form-control" id="inputEmail3" value="<?php echo $student['birthdate']; ?>">
-    </div>
-  </div>
-
-  <div class="form-group">
-    <label for="inputEmail3" class="col-sm-3 control-label">Father's Name</label>
-    <div class="col-sm-9">
-      <input name="fathers_name" type="text" class="form-control" id="inputEmail3" value="<?php echo $student['fathers_name']; ?>">
-    </div>
-  </div>
-
-  <div class="form-group">
-    <label for="inputEmail3" class="col-sm-3 control-label">Mother's Name</label>
-    <div class="col-sm-9">
-      <input name="mothers_name" type="text" class="form-control" id="inputEmail3" value="<?php echo $student['mothers_name']; ?>">
-    </div>
-  </div>
-
-  <div class="form-group">
-    <label for="inputEmail3" class="col-sm-3 control-label">Contact No</label>
-    <div class="col-sm-9">
-      <input name="contact" type="text" class="form-control" id="inputEmail3" value="<?php echo $student['contact']; ?>">
-    </div>
-  </div>
-
-  <div class="form-group">
-    <label for="inputEmail3" class="col-sm-3 control-label">Address</label>
-    <div class="col-sm-9">
-      <textarea name="address" placeholder="Enter Address !" class="form-control" rows="2">
-        <?php echo $student['address']; ?>
-      </textarea>
-    </div>
-  </div>
               </div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary">+ Update Students</button>
+                <button type="submit" class="btn btn-primary">+ Update Member</button>
               </div>
         </form>
             </div>
